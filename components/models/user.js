18f292e9d5024e839c008b44634ae2c2b0ccdc99
Jose Cruz Florian Najera <cflorian@tak.gt>
2023-05-18 15:44:17 -0600

    Backend mantenimiento Create, update, delete

---
+++ components/models/user.js
@@ -0,0 +1,6 @@
+export const User  = {
+    id: 0,
+    email: '',
+    name: '',
+    password: ''
+}
--- pages/api/user.js
+++ pages/api/user.js
@@ -1,47 +1,63 @@
-const express = require('express');
-const app = express();
 const db = require('../../components/database/database');
-const bodyParser = require('body-parser')
-
-
-app.use(bodyParser.json())
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+const User = require('../../components/models/user');
 
 export default async (req, res) => {
 
     console.log(JSON.stringify(req.query))
     if(req.method === 'POST'){
-
+      try {
+        User.email = req.body.email;
+        User.name = req.body.name;
+        User.password = req.body.password;
+        const strQuery =  `INSERT INTO users (email, name, password) VALUES ('${User.email}','${User.name}',crypt('${User.password}', gen_salt('bf', 4)));`;
+        const user = await db.query(strQuery);
+        res.json(user);
+      } catch (error) {
+        console.error(error);
+        res.json(error);
+      }
     }
     else if(req.method === 'PUT'){
-
+      try {
+        User.id = parseInt(req.body.id);
+        User.email = req.body.email;
+        User.name = req.body.name;
+        User.password = req.body.password;
+        const strQuery =  `UPDATE users SET email='${User.email}', name='${User.name}', password=crypt('${User.password}', gen_salt('bf', 4)) WHERE id=${User.id};`;
+        const user = await db.query(strQuery);
+        res.json(user);
+      } catch (error) {
+        console.error(error);
+        res.json(error);
+      }
+    
     }
     else if(req.method === 'DELETE'){
-      
+      try {
+        const idUser = parseInt(req.query.id)
+        const strQuery =  `DELETE FROM users WHERE id=${idUser};`;
+        const user = await db.query(strQuery);
+        res.json(user);
+      } catch (error) {
+        console.error(error);
+        res.json(error);
+      }
+     
     }
     else
     {
       if(JSON.stringify(req.query)=== "{}")
       {
         console.log('All');
-        const posts = await db.query('SELECT * FROM users');
-        res.json(posts.rows);
+        const users = await db.query('SELECT * FROM users');
+        res.json(users.rows);
       }
       else
       {
         console.log(JSON.stringify(req.query.id));
         const idUser = parseInt(req.query.id)
-        const posts = await db.query('SELECT * FROM users where id='+idUser);
-        res.json(posts.rows);
+        const users = await db.query('SELECT * FROM users where id='+idUser);
+        res.json(users.rows);
       }
       
     }
