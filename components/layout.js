92a164af75c211942aa649ded75a53258182ebfd
Jose Cruz Florian Najera <cflorian@tak.gt>
2023-07-14 15:49:16 -0600

    corrections warning alert

--- components/layout.js
+++ components/layout.js
@@ -24,8 +24,7 @@ export default function Layout({ children, home }) {
             />
             <meta name="og:title" content={siteTitle} />
             <meta name="twitter:card" content="summary_large_image" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            
+           
           </Head>
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
